6780eec27fbb4e51f452741168f8db6586cbd584
Mingxing LAI <me@mingxinglai.com>
2017-04-13 17:27:13 +0800

    减少模板渲染需要传递的参数个数

--- headlines.py
+++ headlines.py
@@ -17,11 +17,7 @@ RSS_FEED = {"zhihu": "https://www.zhihu.com/rss",
 def get_news(publication="zhihu"):
     feed = feedparser.parse(RSS_FEED[publication])
     first_content = feed['entries'][0]
-
-    entry = dict(title=first_content.get('title'),
-                 published=first_content.get('published'),
-                 summary=first_content.get('summary'))
-    return render_template('home.html', **entry)
+    return render_template('home.html', article=first_content)
 
 
 if __name__ == '__main__':
