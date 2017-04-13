19a268f843e5160d65a88c4b3ecb9cdab2b1976e
Mingxing LAI <me@mingxinglai.com>
2017-04-13 17:53:12 +0800

    支持用户输入的方式获取headlines

--- headlines.py
+++ headlines.py
@@ -1,7 +1,7 @@
 from __future__ import unicode_literals
 
 import feedparser
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
@@ -13,8 +13,13 @@ RSS_FEED = {"zhihu": "https://www.zhihu.com/rss",
 
 
 @app.route('/')
-@app.route('/<publication>')
-def get_news(publication="zhihu"):
+def get_news():
+    query = request.args.get("publication")
+    if not query or query.lower() not in RSS_FEED:
+        publication = "songshuhui"
+    else:
+        publication = query.lower()
+
     feed = feedparser.parse(RSS_FEED[publication])
     return render_template('home.html', articles=feed['entries'])
 
