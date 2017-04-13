895afbfe8c145b3366dc99aed5a4a2fa911a452a
Mingxing LAI <me@mingxinglai.com>
2017-04-13 17:08:19 +0800

    使用feedparser获取RSS数据

--- headlines.py
+++ headlines.py
@@ -1,12 +1,29 @@
+from __future__ import unicode_literals
+
+import feedparser
 from flask import Flask
 
 
 app = Flask(__name__)
 
+ZHIHU_FEED = "https://www.zhihu.com/rss"
+
 
 @app.route('/')
 def get_news():
-    return "no news is good news"
+    feed = feedparser.parse(ZHIHU_FEED)
+    first_content = feed['entries'][0]
+    html_format = """
+    <html> <body>
+        <h1> Zhihu Headlines </h1>
+        <b> {0} </b> <br/>
+        <i> {1} </i> <br/>
+        <p> {2} </p> <br/>
+    <body> </html>"""
+
+    return html_format.format(first_content.get('title'),
+                              first_content.get('published'),
+                              first_content.get('summary'))
 
 
 if __name__ == '__main__':
